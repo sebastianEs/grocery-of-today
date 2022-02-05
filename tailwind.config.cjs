142d1495ec3925e1b20d9cd0787b502ffec7ee42
sebastian estrada <sebastian@queenslab.se>
2022-02-05 20:59:35 +0100

    initial commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,21 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  purge: ['./src/**/*.svelte', './src/**/*.css'],
+  darkMode: false,
   theme: {
+    colors: {
+      transparent: 'transparent',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      'svelte-prime': '#ff5030',
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
