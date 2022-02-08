81a0e3386862cca868cd1c4fa7e6b176ad582073
sebastian estrada <sebastian@queenslab.se>
2022-02-08 14:46:43 +0100

    setup tailwind css

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,7 @@
+<script>
+  import "../app.css";
+</script>
+
+<div class="container mx-auto max-w-lg h-full" >
+  <slot></slot>
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>
+  let currentText = "";
+
+  const handleClick = () => {
+
+  }
+</script>
+
+<div class="container" >
+
+  <input placeholder="Enter a grocery" />
+  <button on:click={handleClick} >Add</button>
+
+</div>
+
+<style>
+  .container {
+    background-color: aqua;
+    margin: 0;
+    padding: 0;
+  } 
+</style>
